947c0e93143902c83e089891da935f8822e9e10c
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 22:13:12 +0900

    2-9

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -3,10 +3,8 @@ package lesson2;
 public class Main {
 
 	public static void main(String[] args) {
-		double d = 8.5 / 2;
-		long l = 2147483648L;
-		System.out.println(d);
-		System.out.println(l);
+		String msg = "私の年齢は" + 23;
+		System.out.println(msg);
 	}
 
 }
