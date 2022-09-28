c56b076c1ef18d7feda4502cc4f6254519b24b1e
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 21:53:52 +0900

    2-4(インクリメント・デクリメント演算子を他の演算子と利用)

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -3,9 +3,10 @@ package lesson2;
 public class Main {
 
 	public static void main(String[] args) {
-		int a = 100;
-		a++;
-		System.out.println(a);
+		int a = 10;
+		int b = 10;
+		System.out.println(++a + 50);
+		System.out.println(b++ + 50);
 	}
 
 }
