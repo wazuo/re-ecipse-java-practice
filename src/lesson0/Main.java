a4931090e254aad5f4f1be029bf54514ccc5e329
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 10:59:20 +0900

    コード0-6

--- src/lesson0/Main.java
+++ src/lesson0/Main.java
@@ -3,10 +3,13 @@ package lesson0;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(35 - 10);
-		System.out.println(-5 * 2);
-		System.out.println(6 * 6 * 3.14);
-		System.out.println("こたえは" + 64);
+		System.out.println("すがわら");
+		System.out.println("31歳です");
+		System.out.println("お酒が好きです");
+		System.out.println(31 + 31);
+		int x;
+		x = 6;
+		System.out.println(x);
 	}
 
 }
