1e08461959f2c1d56cbe2bb3403b686e138d34d6
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 10:54:21 +0900

    コード0-5(計算結果)

--- src/lesson0/Main.java
+++ src/lesson0/Main.java
@@ -3,11 +3,10 @@ package lesson0;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("すがわら");
-		System.out.println("31歳です");
-		System.out.println("お酒が好きです");
-		System.out.println("31+31の計算をします");
-		System.out.println(31+31);
+		System.out.println(35 - 10);
+		System.out.println(-5 * 2);
+		System.out.println(6 * 6 * 3.14);
+		System.out.println("こたえは" + 64);
 	}
 
 }
