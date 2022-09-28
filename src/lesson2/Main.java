0e3b08b2cf101390018497ed597bb3e66d08b1b3
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 22:49:38 +0900

    lesson2練習問題1

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -1,15 +1,12 @@
 package lesson2;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("あなたの名前を入力して下さい。");
-		String name = new Scanner(System.in).nextLine();
-		System.out.println("あなたの年齢を入力してください。");
-		int age = new Scanner(System.in).nextInt();
-		System.out.println("ようこそ、" + age + "歳の" + name + "さん");
+		int x = 5;
+		int y = 10;
+		String ans = "x+yは" +(x + y);
+		System.out.println(ans);
 	}
 
 }
