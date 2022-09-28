e9dd00e005f08ea15d64365ba737a7c9549c0b01
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 11:40:39 +0900

    コード1-4

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -3,10 +3,14 @@ package lesson1;
 public class Main {
 
 	public static void main(String[] args) {
-		int age = 20;
-		System.out.println("私の年齢は" + age);
-		age = 31;
-		System.out.println("…いや、本当の年齢は" + age);
+		double pi = 3.14;
+		int pie = 5;
+		System.out.println("半径" + pie + "cmのパイの面積は、");
+		System.out.println(pie * pie * pi);
+		System.out.println("パイの半径を倍にします");
+		pi = 10;
+		System.out.println("半径" + pie + "cmのパイの面積は、");
+		System.out.println(pie * pie * pi);
 	}
 
 }
