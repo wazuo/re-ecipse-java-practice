a088bacdcbf08002df1403a999d198d0c8a30feb
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 11:30:45 +0900

    byte,short,int long型の利用

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -3,9 +3,18 @@ package lesson1;
 public class Main {
 
 	public static void main(String[] args) {
-		int age;
-		age = 30;
+		byte glasses;
+		glasses = 2;
+		short age;
+		age = 18;
+		int salary;
+		salary = 152000;
+		long worldPeople;
+		worldPeople = 6900000000L;
+		System.out.println(glasses);
 		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(worldPeople);
 	}
 
 }
