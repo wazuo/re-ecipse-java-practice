24dc3392c96cc961719f81176518d57f1491e9a7
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 16:04:23 +0900

    2-1

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -3,8 +3,12 @@ package lesson2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		int a;
+		int b;
+		a = 20;
+		b = a + 5;
+		System.out.println(a);
+		System.out.println(b);
 	}
 
 }
