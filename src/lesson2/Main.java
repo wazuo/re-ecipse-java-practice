1181da507cf9a59a6e2c39c420e0c923ab9d863c
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 22:17:10 +0900

    2-10(変数の初期化の利用)

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		String name = "すがわら";
-		String message;
-		message = name + "さん、こんにちは";
+		String message = name + "さん、こんにちは";
 		System.out.println(message);
 	}
 
