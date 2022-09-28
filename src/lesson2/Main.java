7baa9a44575dcb7f7f0987777ea79f39218d3434
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 22:18:20 +0900

    2-10(直接命令文に記入)

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		String name = "すがわら";
-		String message = name + "さん、こんにちは";
-		System.out.println(message);
+		System.out.println(name + "さん、こんにちは");
 	}
 
 }
