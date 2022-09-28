4dd26bf96c17cb078135602412ee0d2a99f480ab
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 15:59:37 +0900

    lesson1練習問題3

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -3,10 +3,16 @@ package lesson1;
 public class Main {
 
 	public static void main(String[] args) {
-		int a = 3;
-		int b = 5;
-		int c = a * b;
-		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+		boolean isError = true;
+		char initial = '駆';
+		double pi = 3.14;
+		long number = 314159265853979L;
+		String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+		System.out.println(isError);
+		System.out.println(initial);
+		System.out.println(pi);
+		System.out.println(number);
+		System.out.println(msg);
 	}
 
 }
