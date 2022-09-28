5a3ab7ac52eca999bfeb70804888720ede9d2865
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 21:57:00 +0900

    2-5

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -3,10 +3,10 @@ package lesson2;
 public class Main {
 
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 10;
-		System.out.println(++a + 50);
-		System.out.println(b++ + 50);
+		float f = 3;
+		double d = f;
+		System.out.println(f);
+		System.out.println(d);
 	}
 
 }
