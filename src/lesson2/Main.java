d90b099056b9f5c439c2d06ec117ecac10d52d62
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 22:32:42 +0900

    2-14(インポート文の利用)

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -1,9 +1,11 @@
 package lesson2;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int r = new java.util.Random().nextInt(90);
+		int r = new Random().nextInt(90);
 		System.out.println("あなたはたぶん、" + r + "歳ですね？");
 	}
 
