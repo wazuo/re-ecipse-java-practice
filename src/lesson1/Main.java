6d61cb29138c74326bbe865cd274e47280fe4ece
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 11:42:33 +0900

    コード1-5

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -3,14 +3,16 @@ package lesson1;
 public class Main {
 
 	public static void main(String[] args) {
-		double pi = 3.14;
+//		円の円周率
+		final double PI = 3.14;
+//		円の半径
 		int pie = 5;
 		System.out.println("半径" + pie + "cmのパイの面積は、");
-		System.out.println(pie * pie * pi);
+		System.out.println(pie * pie * PI);
 		System.out.println("パイの半径を倍にします");
-		pi = 10;
+		PI = 10;
 		System.out.println("半径" + pie + "cmのパイの面積は、");
-		System.out.println(pie * pie * pi);
+		System.out.println(pie * pie * PI);
 	}
 
 }
