d80cb158b999557b662ff5acc7a22006b329207c
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 10:44:02 +0900

    コード0-3(エラー)

--- src/lesson0/Main.java
+++ src/lesson0/Main.java
@@ -1,9 +1,8 @@
 package lesson0;
 
 public class Main {
+	  public static void main(String[] args) {
+	    System.out.println("湊くんかっこいい！最高！);
 
-	public static void main(String[] args) {
-		System.out.println("すがわら");
+	  }
 	}
-
-}
