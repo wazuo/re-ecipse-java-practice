98aefce68910eedf187314752540f069c60b34ee
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 22:32:01 +0900

    2-14

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -3,9 +3,8 @@ package lesson2;
 public class Main {
 
 	public static void main(String[] args) {
-		String age = "31";
-		int n = Integer.parseInt(age);
-		System.out.println("あなたは来年、" + (n + 1) + "歳になりますよね");
+		int r = new java.util.Random().nextInt(90);
+		System.out.println("あなたはたぶん、" + r + "歳ですね？");
 	}
 
 }
