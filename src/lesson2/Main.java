d6c9d3dbf37aac85d4354723b974fda425c7b85e
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 21:58:07 +0900

    2-6(エラー)

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -3,10 +3,7 @@ package lesson2;
 public class Main {
 
 	public static void main(String[] args) {
-		float f = 3;
-		double d = f;
-		System.out.println(f);
-		System.out.println(d);
+		int i = 3.2;
 	}
 
 }
