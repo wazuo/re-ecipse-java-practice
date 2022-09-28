837fe733c723b16ef78f5943c21ee047608a42c4
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 10:52:06 +0900

    コード0-5

--- src/lesson0/Main.java
+++ src/lesson0/Main.java
@@ -6,6 +6,8 @@ public class Main {
 		System.out.println("すがわら");
 		System.out.println("31歳です");
 		System.out.println("お酒が好きです");
+		System.out.println("31+31の計算をします");
+		System.out.println(31+31);
 	}
 
 }
