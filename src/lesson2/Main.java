69b7ff2a39d6c68c4f5415eff3bfe3498f1f1daa
wazuo <95359918+wazuo@users.noreply.github.com>
2022-09-28 22:46:08 +0900

    2-15(インポート文の利用)

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -1,13 +1,15 @@
 package lesson2;
 
+import java.util.Scanner;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("あなたの名前を入力してください");
-    @SuppressWarnings("resource")
-	String name = new java.util.Scanner(System.in).nextLine();
-    System.out.println("あなたの年齢を入力してください");
-    @SuppressWarnings("resource")
-	int age = new java.util.Scanner(System.in).nextInt();
-    System.out.println("ようこそ、" + age + "歳の" + name + "さん");
-  }
+
+	public static void main(String[] args) {
+		System.out.println("あなたの名前を入力して下さい。");
+		String name = new Scanner(System.in).nextLine();
+		System.out.println("あなたの年齢を入力してください。");
+		int age = new Scanner(System.in).nextInt();
+		System.out.println("ようこそ、" + age + "歳の" + name + "さん");
+	}
+
 }
